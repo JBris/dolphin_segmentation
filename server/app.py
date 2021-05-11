1132370e7c938a3862cee03cba86b32ede535bd2
JBris <james.a.bristow.contact@gmail.com>
2021-05-11 18:29:02 +1200

    Adding celery worker

--- server/app.py
+++ server/app.py
@@ -4,9 +4,15 @@ from decouple import config
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
+from api.services.celery import make_celery
 from api.services.validation.file import FileSelectValidator, FileListValidator, FilePathValidator
 
 app = Flask(__name__)
+app.config.update(
+    CELERY_BROKER_URL = config('CELERY_BROKER_URL', default = 'redis://redis:6379/0'),
+    CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND', default = 'redis://redis:6379/0')
+)
+celery = make_celery(app)
 CORS(app)
 
 @app.route('/', methods=['GET'])
@@ -29,6 +35,8 @@ def file_select():
     validator = FilePathValidator()
     data = validator.validate(data)
     if data is None: return jsonify(validator.get_error_message()), 400 
+
+
     return jsonify(data)
 
 if __name__ == '__main__':
