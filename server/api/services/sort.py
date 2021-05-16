ab5e8f9127e418e3a4e6b6a308e8d3ce3cd87cbb
JBris <james.a.bristow.contact@gmail.com>
2021-05-16 17:00:52 +1200

    Fixing sort

--- server/api/services/sort.py
+++ server/api/services/sort.py
@@ -4,12 +4,17 @@ import shutil
 class Sort:
     def sort(self, data, out, include_metadata = False):
         Path(out).mkdir(parents = True, exist_ok = True)
+        for c in data["class"].unique(): Path(f"{out}/{c}").mkdir(parents = True, exist_ok = True)
+        
         if include_metadata: copy = shutil.copy2
         else: copy = shutil.copy
 
         files = {}
-        for file in data["file"]:
-            try: files[file] = copy(file, out)
+        def move_files(row):
+            file = row["file"]
+            try: files[file] = copy(file, f"{out}/{row['class']}")
             except: files[file] = ""
+        data.apply(move_files, axis = 1)
+
         return files
         
